f28a091199ebd90ea983d72b48843c893fded0b3
Joe Sweeney <joe@j1.io>
2020-07-29 19:54:41 -0400

    Add more failing tests

--- test/test_test.exs
+++ test/test_test.exs
@@ -2,16 +2,16 @@ defmodule TestTest do
   use ExUnit.Case
   doctest Test
 
-  test "greets the world" do
-    assert Test.hello() == :world
-  end
-
   test "1 == 2" do
     assert 1 == 2
   end
 
   test "throws" do
     throw "Throw!"
+  end
+
+  test "greets the world" do
+    assert Test.hello() == :world
   end
 
   test "raises" do
@@ -24,5 +24,17 @@ defmodule TestTest do
 
   test "internal function raises with stacktrace" do
     assert Test.raise_stacktrace() == :ok
+  end
+
+  test "multiple failing" do
+    assert 1 == 2
+    assert 1 == 3
+    assert 1 == 4
+  end
+
+  test "some passing some failing" do
+    assert 1 == 2
+    assert 1 == 1
+    assert 1 == 4
   end
 end
