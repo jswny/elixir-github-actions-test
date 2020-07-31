35b6f502f5f52b6c94f0edfb8b42d11b981f4487
Joe Sweeney <joe@j1.io>
2020-07-31 18:02:34 -0400

    Don't loop

--- test/test_test.exs
+++ test/test_test.exs
@@ -19,22 +19,22 @@ defmodule TestTest do
   end
 
   test "internal function raises" do
-    assert Test.raise() == :ok
+    assert Test.raise() == :okay
   end
 
   test "internal function raises with stacktrace" do
-    assert Test.raise_stacktrace() == :okay
+    assert Test.raise_stacktrace() == :ok
   end
 
   test "multiple failing" do
     assert 1 == 2
-    assert 1 == 2
+    assert 1 == 3
     assert 1 == 4
   end
 
   test "some passing some failing" do
     assert 1 == 2
-    assert 1 == 1
+    assert 1 == 2
     assert 1 == 4
   end
 end
